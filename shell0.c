ab1b0583703682f2503900735858a7d3706c2496
Raniah06 <raniafadil06@gmail.com>
2023-12-13 03:56:30 -0800

    m

--- shell0.c
+++ shell0.c
@@ -1,33 +1,26 @@
 #include "shell.h"
 #include "string.h"
 
-/**
- * main - main body of the shell
- * Return: 0
- */
-
 int main(void) {
     char buffer[BUFFER_SIZE];
     ssize_t read_bytes;
-    char *args[] = {buffer, NULL};
+    char *args[2];
+    pid_t child_pid;
 
     while (1) {
-        // Display the prompt
         write(STDOUT_FILENO, "$ ", 2);
 
-        // Read user input
         read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
         if (read_bytes <= 0) {
-            // End of file condition (Ctrl+D or error)
             write(STDOUT_FILENO, "\n", 1);
             break;
         }
-
-        // Null-terminate the input
         buffer[read_bytes - 1] = '\0';
 
-        // Fork a child process
-        pid_t child_pid = fork();
+        args[0] = buffer;
+        args[1] = NULL;
+
+        child_pid = fork();
 
         if (child_pid == -1) {
             perror("fork");
@@ -35,13 +28,10 @@ int main(void) {
         }
 
         if (child_pid == 0) {
-            // Child process
             execve(args[0], args, NULL);
-            // If execve fails
             perror("execve");
             exit(EXIT_FAILURE);
         } else {
-            // Parent process
             wait(NULL);
         }
     }
