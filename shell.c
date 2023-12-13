96aebdaaa50219ee8a97a47cb26fb1d915cea747
Raniah06 <raniafadil06@gmail.com>
2023-12-13 05:01:58 -0800

    m

--- shell.c
+++ shell.c
@@ -1,10 +1,18 @@
 #include "shell.h"
+#include "string.h"
+
+/**
+ * main - main body of the shell
+ * Return: 0
+ */
 
 int main(void) {
     char buffer[BUFFER_SIZE];
     ssize_t read_bytes;
-    char *args[2];
     pid_t child_pid;
+    char *args[64];
+    int arg_count;
+    char *token;
 
     while (1) {
         write(STDOUT_FILENO, "$ ", 2);
@@ -14,10 +22,16 @@ int main(void) {
             write(STDOUT_FILENO, "\n", 1);
             break;
         }
-        buffer[read_bytes - 1] = '\0';
-
         args[0] = buffer;
-        args[1] = NULL;
+        buffer[read_bytes - 1] = '\0';
+        arg_count = 0;
+        token = strtok(buffer, " ");
+
+        args[arg_count++] = token;
+        while ((token = strtok(NULL, " ")) != NULL) {
+            args[arg_count++] = token;
+        }
+        args[arg_count] = NULL;
 
         child_pid = fork();
 
@@ -27,14 +41,17 @@ int main(void) {
         }
 
         if (child_pid == 0) {
-            execve(args[0], args, NULL);
-            perror("execve");
-            exit(EXIT_FAILURE);
+            if (access(args[0], X_OK) == 0) {
+                execve(args[0], args, NULL);
+                exit(EXIT_FAILURE);
+            } else {
+                exit(EXIT_FAILURE);
+            }
         } else {
             wait(NULL);
         }
     }
 
-    return 0;
+    return (0);
 }
 
